828857b62be1c176bf9798be463c3f5debfa1f01
Tomasz Wirkus <t.wirkus@i4b.pl>
2021-04-04 12:56:39 +0200

    Started designing details view

--- src/styles/detailsBookView.ts
+++ src/styles/detailsBookView.ts
@@ -16,3 +16,27 @@ export const BookDetails = styled.section`
     flex-direction: row;
   }
 `;
+
+export const BookPhoto = styled.img`
+  height: 320px;
+  width: 212px;
+`;
+
+export const BookInformations = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 1em 2.5em;
+`;
+
+export const Description = styled.div`
+  width: 100%;
+  margin-top: 2em;
+  font-size: 14px;
+`;
+
+export const RateBook = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 2em;
+`;
--- src/views/DetailsBookView/index.tsx
+++ src/views/DetailsBookView/index.tsx
@@ -3,7 +3,14 @@ import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "src/store";
 import { clearBook, getBook } from "src/slices/books";
 import { PageContainer, PageHeading, Flex } from "src/styles/layout";
-import { BookDetails } from "src/styles/detailsBookView";
+import {
+  BookDetails,
+  BookInformations,
+  BookPhoto,
+  Description,
+  RateBook,
+} from "src/styles/detailsBookView";
+import { Typography } from "@material-ui/core";
 
 const DetailsBookView = () => {
   const dispatch = useDispatch();
@@ -23,7 +30,32 @@ const DetailsBookView = () => {
     <>
       <PageContainer>
         <PageHeading>{book.title}</PageHeading>
-        <BookDetails></BookDetails>
+        <BookDetails>
+          <BookPhoto src={require(`../../${book.photo}`).default} alt="book" />
+          <BookInformations>
+            <Typography variant="body1">Author: {book.author}</Typography>
+            <Flex>
+              <Typography variant="body2" style={{ marginRight: "10px" }}>
+                Release Year: {book.year}
+              </Typography>
+              <Typography variant="body2">
+                Number of pages: {book.pages}
+              </Typography>
+            </Flex>
+            <Description>
+              Description: Curabitur sed felis urna. Proin in nisl non dolor
+              aliquam aliquam et hendrerit dui. Proin in sagittis ligula, et
+              luctus massa. Suspendisse vehicula nulla eu elit pulvinar, quis
+              finibus lacus pharetra. Maecenas vulputate scelerisque arcu.
+              Quisque eget est non metus ultrices porta ullamcorper non risus.
+              Mauris finibus, ante quis vulputate mattis, lectus lorem lobortis
+              lacus, sed efficitur lorem mi condimentum nisl.
+            </Description>
+            <RateBook>
+              <Typography variant="body2">Rate Book:</Typography>
+            </RateBook>
+          </BookInformations>
+        </BookDetails>
       </PageContainer>
     </>
   );
